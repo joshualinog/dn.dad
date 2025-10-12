be663b201d5fc6a550a9cfdf468c233613fe5d98
Joshua Linog <jsinkcreations@gmail.com>
2025-10-11 21:40:58 -0400

    add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ["Rubik", "ui-sans-serif", "system-ui", "sans-serif"],
+        dynapuff: ["Dynapuff", "cursive"],
+        gaegu: ["Gaegu", "cursive"],
       },
     },
   },
